667bc96361196ce100bc604205ec6a8f832f687d
Vicky Loebel <vickyloebel@gmail.com>
2021-04-22 09:36:03 -0700

    added mainfest to cached files

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,21 +1,22 @@
 const APP_PREFIX = 'Budget-';
-//? capital B Budget above?  
-const VERSION = 'version_02';  // cache version
+// changing cache version forces refresh
+const VERSION = 'version_03';  
 const CACHE_NAME = APP_PREFIX + VERSION
 const FILES_TO_CACHE = [
   "/",
   "/index.html",
+  "/manifest.json",
   "/css/styles.css",
-  "/js/idb.js",
-  "/js/index.js",
-  "icons/icon-512x512.png",
-  "icons/icon-384x384.png",
-  "icons/icon-192x192.png",
-  "icons/icon-152x152.png",
-  "icons/icon-144x144.png",
-  "icons/icon-128x128.png",
+  "icons/icon-72x72.png",
   "icons/icon-96x96.png",
-  "icons/icon-72x72.png"
+  "icons/icon-128x128.png",
+  "icons/icon-144x144.png",
+  "icons/icon-152x152.png",
+  "icons/icon-192x192.png",
+  "icons/icon-384x384.png",
+  "icons/icon-512x512.png",
+  "/js/idb.js",
+  "/js/index.js"
 ];
 
 // Respond with cached resources
